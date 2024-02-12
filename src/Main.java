7ead5f364ff7c3007f872c0a868d9222576bf385
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-02-12 18:05:35 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String dateString = "2023-03-01T13:00:00Z";
+        OffsetDateTime dateTime = parseDate(dateString);
+
+        String dataString = formatLocale(dateTime);
+        System.out.println(dataString);
+
+    }
+
+    static String formatLocale(OffsetDateTime dateTime) {
+        return dateTime.format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALY));
+    }
+
+    static OffsetDateTime parseDate(String dateString) {
+        return OffsetDateTime.parse(dateString);
     }
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,4 +1,29 @@
-import static org.junit.jupiter.api.Assertions.*;
-class MainTest {
-  
+import org.junit.Test;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testParseDate() {
+        String dateString = "2002-03-01T13:00:00Z";
+        OffsetDateTime dateTime = Main.parseDate(dateString);
+        OffsetDateTime expectedDate = OffsetDateTime.parse(dateString);
+
+        assertEquals(expectedDate, dateTime);
+    }
+
+    @Test
+    public void testFormatLocale() {
+        OffsetDateTime dateTime = OffsetDateTime.parse("2002-03-01T13:00:00Z");
+        String formattedLocale = Main.formatLocale(dateTime);
+        String expectedLocale = dateTime.format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALY));
+
+        assertEquals(expectedLocale, formattedLocale);
+    }
+
 }
